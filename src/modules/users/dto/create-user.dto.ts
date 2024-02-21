42cd25952d9b49ec460d7bf01a16ac54bb636658
Gabriel Camargo <gcamargo.dev@gmail.com>
2024-02-21 20:41:30 -0300

    feat: create user dto

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,24 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty({ message: 'Name is required' })
+  name: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Password is required' })
+  password: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Email is required' })
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsOptional()
+  profilePicture: string;
+
+  @IsString()
+  @IsOptional()
+  description: string;
+}
