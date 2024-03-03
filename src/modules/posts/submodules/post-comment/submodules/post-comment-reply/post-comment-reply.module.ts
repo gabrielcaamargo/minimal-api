fffa6006550deeef5aae6ac20bcfab4737998b10
Gabriel Camargo <gcamargo.dev@gmail.com>
2024-03-03 20:33:00 -0300

    feat: create post comment reply create method on service

--- src/modules/posts/submodules/post-comment/submodules/post-comment-reply/post-comment-reply.module.ts
+++ src/modules/posts/submodules/post-comment/submodules/post-comment-reply/post-comment-reply.module.ts
@@ -4,9 +4,10 @@ import { PostCommentReplyController } from './post-comment-reply.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostComment } from 'src/modules/posts/entities/post-comment.entity';
 import { PostCommentReply } from 'src/modules/posts/entities/post-comment-reply.entity';
+import { User } from 'src/modules/users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PostComment, PostCommentReply])],
+  imports: [TypeOrmModule.forFeature([PostComment, PostCommentReply, User])],
   controllers: [PostCommentReplyController],
   providers: [PostCommentReplyService],
 })
--- src/modules/posts/submodules/post-comment/submodules/post-comment-reply/post-comment-reply.service.ts
+++ src/modules/posts/submodules/post-comment/submodules/post-comment-reply/post-comment-reply.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PostCommentReply } from 'src/modules/posts/entities/post-comment-reply.entity';
 import { PostComment } from 'src/modules/posts/entities/post-comment.entity';
 import { Repository } from 'typeorm';
+import { CreatePostCommentReplyDto } from '../../dto/create-post-comment-reply.dto';
+import { User } from 'src/modules/users/entities/user.entity';
 
 @Injectable()
 export class PostCommentReplyService {
@@ -11,5 +13,40 @@ export class PostCommentReplyService {
     private readonly postCommentRepo: Repository<PostComment>,
     @InjectRepository(PostCommentReply)
     private readonly postCommentReplyRepo: Repository<PostCommentReply>,
+    @InjectRepository(User)
+    private readonly userRepo: Repository<User>,
   ) {}
+
+  async create(
+    createPostCommentReplyDto: CreatePostCommentReplyDto,
+    loggedUserId: string,
+  ) {
+    const { repliedComment } = createPostCommentReplyDto;
+
+    const comment = await this.postCommentRepo.findOne({
+      where: {
+        id: repliedComment.id,
+      },
+    });
+
+    if (!comment) {
+      throw new NotFoundException('Comment not found');
+    }
+
+    const user = await this.userRepo.findOne({
+      where: {
+        id: loggedUserId,
+      },
+    });
+
+    const reply = this.postCommentReplyRepo.create(createPostCommentReplyDto);
+    reply.repliedComment = comment;
+    reply.author = user;
+
+    await this.postCommentRepo.update(comment.id, {
+      repliesCount: comment.repliesCount++,
+    });
+
+    return await this.postCommentReplyRepo.save(reply);
+  }
 }
